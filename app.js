3ed0d54d5bf2abea0e86ab9a0ba9f955c06fddee
mervyn15 <mervyn27@gmail.com>
2021-05-20 04:03:01 +0800

    gpo

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const postsRoute = require('./routes/posts');
+const userRoute = require('./routes/user');
+const commentsRoute = require('./routes/comments');
+const imageRoute = require('./routes/images');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-});
+app.use(bodyParser.json());
+app.use('/uploads', express.static('uploads'));
 
-module.export = app;
+app.use("/posts", postsRoute);
+app.use("/user", userRoute);
+app.use("/comments", commentsRoute);
+app.use("/images", imageRoute);
+
+module.exports = app;
